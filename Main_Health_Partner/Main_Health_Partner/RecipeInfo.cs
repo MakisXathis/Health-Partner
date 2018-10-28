b253af2d28124149b22eab2a162c3925934b4ec2
MakisXathis <MNB1000@yahoo.gr>
2018-10-28 12:20:00 +0200

    Week Meal completed

---
+++ Main_Health_Partner/Main_Health_Partner/RecipeInfo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Health_Partner
+{
+    class RecipeInfo
+    {
+        List<String> Names = new List<String>();
+        String Steps { get; set; }
+
+        public RecipeInfo(List<String> n, String s)
+        {
+            Names = n;
+            Steps = s;
+        }
+
+        public List<String> getIngredients()
+        {
+            return Names;
+        }
+
+        public String getSteps()
+        {
+            return Steps;
+        }
+
+        public void setIngredients(List<String> n)
+        {
+            Names = n;
+        }
+
+        public void setSteps(String s)
+        {
+            Steps = s;
+        }
+
+        public void ToString()
+        {
+            String s = "Ingredients : ";
+            for (int i = 0; i < Names.Count; i++)
+            {
+                String ingredient = Names[i] + ", ";
+                s = s + ingredient;
+            }
+
+            Console.WriteLine(s);
+            Console.WriteLine(Steps);
+        }
+
+    }
+}
